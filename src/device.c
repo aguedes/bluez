a14ad05c70dd3ca3fad40f421a653e1729eeaee1
Claudio Takahasi <claudio.takahasi@openbossa.org>
2013-05-15 14:42:41 -0300

    core: Remove disable_auto_connect
    This patch removes the variable disable_auto_connect used to control
    Bluetooth Low Energy scanning. Close the L2CAP ATT socket should provide
    the same control mechanism.

--- src/device.c
+++ src/device.c
@@ -198,7 +198,6 @@ struct btd_device {
 	gboolean	paired;
 	gboolean	blocked;
 	gboolean	bonded;
-	gboolean	disable_auto_connect;
 	gboolean	general_connect;
 
 	bool		legacy;
@@ -1069,6 +1068,13 @@ void device_request_disconnect(struct btd_device *device, DBusMessage *msg)
 	if (device->browse)
 		browse_request_cancel(device->browse);
 
+	if (device->att_io) {
+		/* Connection attempt pending */
+		g_io_channel_shutdown(device->att_io, FALSE, NULL);
+		g_io_channel_unref(device->att_io);
+		device->att_io = NULL;
+	}
+
 	if (device->connect) {
 		DBusMessage *reply = btd_error_failed(device->connect,
 								"Cancelled");
@@ -1116,12 +1122,6 @@ static DBusMessage *disconnect(DBusConnection *conn, DBusMessage *msg,
 
 	if (!device->connected)
 		return btd_error_not_connected(msg);
-
-	/*
-	 * Disable connections through passive scanning until
-	 * Device1.Connect is called
-	 */
-	device->disable_auto_connect = TRUE;
 
 	device_request_disconnect(device, msg);
 
@@ -1329,8 +1329,6 @@ static DBusMessage *dev_connect(DBusConnection *conn, DBusMessage *msg,
 			return dbus_message_new_method_return(msg);
 
 		device_set_temporary(dev, FALSE);
-
-		dev->disable_auto_connect = FALSE;
 
 		err = connect_le(dev);
 		if (err < 0)
@@ -3065,14 +3063,6 @@ static void store_services(struct btd_device *device)
 	g_key_file_free(key_file);
 }
 
-static bool device_get_auto_connect(struct btd_device *device)
-{
-	if (device->disable_auto_connect)
-		return false;
-
-	return device->attios ? true : false;
-}
-
 static void attio_connected(gpointer data, gpointer user_data)
 {
 	struct attio_data *attio = data;
@@ -3106,7 +3096,7 @@ static gboolean attrib_disconnected_cb(GIOChannel *io, GIOCondition cond,
 
 	g_slist_foreach(device->attios, attio_disconnected, NULL);
 
-	if (!device_get_auto_connect(device)) {
+	if (device->attios == NULL) {
 		DBG("Automatic connection disabled");
 		goto done;
 	}
@@ -3329,7 +3319,7 @@ static void att_error_cb(const GError *gerr, gpointer user_data)
 	if (g_error_matches(gerr, BT_IO_ERROR, ECONNABORTED))
 		return;
 
-	if (device_get_auto_connect(device)) {
+	if (device->attios) {
 		DBG("Enabling automatic connections");
 		connect_le(device);
 	}
