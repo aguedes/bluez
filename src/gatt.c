c40687c4a74371389ffc161dd9a5e6ade541677d
Claudio Takahasi <claudio.takahasi@openbossa.org>
2013-07-02 16:55:13 -0300

    gatt: Fix memory leak of g_key_file_to_data
    ==16007==    at 0x4A08A0E: realloc (vg_replace_malloc.c:662)
    ==16007==    by 0x3CA5E4D4A6: g_realloc (gmem.c:224)
    ==16007==    by 0x3CA5E65176: g_string_maybe_expand (gstring.c:102)
    ==16007==    by 0x3CA5E66524: g_string_append_vprintf (gstring.c:1132)
    ==16007==    by 0x3CA5E66726: g_string_append_printf (gstring.c:1232)
    ==16007==    by 0x3CA5E40F7C: g_key_file_to_data (gkeyfile.c:1486)
    ==16007==    by 0x4670DE: database_store (gatt.c:1389)

--- src/gatt.c
+++ src/gatt.c
@@ -1457,6 +1457,7 @@ static void database_store(struct btd_device *device, GList *database)
 			create_file(filename, S_IRUSR | S_IWUSR);
 			g_file_set_contents(filename, data, len, NULL);
 		}
+		g_free(data);
 	}
 
 	g_key_file_free(key_file);
